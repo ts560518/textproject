5d9077f8f3611bc8c4bef6c998a8f9965c95ced9
ts5605 <48780788+ts5605@users.noreply.github.com>
2020-11-26 21:55:47 +0900

    push, pop, peek

---
+++ src/kr/co/stack/Stack.java
@@ -0,0 +1,40 @@
+package kr.co.stack;
+
+public class Stack {
+	
+	private int stack_size =20;
+	private int top;
+	private int stackArr[];
+	
+	public Stack() {
+		top = -1;
+		this.stack_size = stack_size;
+		stackArr = new int[stack_size];
+	}
+	
+	public int push(int data) {
+		if(top>=20) {	// 스택 꽉참
+			return 1;
+		}
+		stackArr[top++] = data;
+		return 0;
+	}
+	
+	public int pop() {
+		int data;
+		if(top == -1) {
+			return 0;
+		}
+		data = stackArr[top];
+		top--;		
+		return data;
+	}
+	
+	public int peek() {
+		if(top != -1) {
+			return stackArr[top];
+		}
+		return 0;
+	}
+	
+}
